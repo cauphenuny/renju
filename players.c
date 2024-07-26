d9ac3198cbaca29bb5d533396ba74227e3446967
cauphenuny <starseekist@gmail.com>
2024-07-26 22:22:13 +0800

    feat: add virtual moves (v1.1.1)

--- players.c
+++ players.c
@@ -18,9 +18,9 @@ point_t move(int player_type, const board_t board, int id) {
         }
         case MCTS: {
             mcts_parm_t parm = {.C = 1.414, .M = 10};
-            if (id == 2) {
-                parm.M = 10000;
-            }
+            //if (id == 2) {
+            //    parm.M = 10000;
+            //}
             return mcts(board, id, parm);
         }
     }
